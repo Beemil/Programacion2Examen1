7299e92050cb3d4a97191daf94e3326463416caf
Beemil <valeskavr17@gmail.com>
2025-08-20 19:22:57 -0600

    Creacion Class asignatura

---
+++ Programación2Examen1/Asignatura.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace Programacion2Examen1
+{
+    public class Asignatura
+    {
+        public string NombreAsignatura { get; set; }
+        public string NombreDocente { get; set; }
+        public string Horario { get; set; }
+
+        public Asignatura() { }
+
+        public Asignatura(string nombreAsignatura, string nombreDocente, string horario)
+        {
+            NombreAsignatura = nombreAsignatura;
+            NombreDocente = nombreDocente;
+            Horario = horario;
+        }
+    }
+}
